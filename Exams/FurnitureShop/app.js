a7db9561e9a220167b27c2eb274cb2d1604be5d7
lakrizz1987 <ignatov.ivaylo@gmail.com>
2022-06-05 10:57:14 +0300

    Exams/FurnitureShop/app.js

--- Exams/FurnitureShop/app.js
+++ Exams/FurnitureShop/app.js
@@ -10,29 +10,52 @@ function solve() {
     let addBtn = document.getElementById('add');
     let tbody = document.getElementById('furniture-list');
     let totalPrice = document.querySelector('td.total-price');
-
-    addBtn.addEventListener('click',(e)=>{
+    let totalSum = 0;
+    addBtn.addEventListener('click', (e) => {
         e.preventDefault()
-        if(model.value != '' && year.value != '' && description.value != '' && price.value != ''){
-            if( year.value > 0  && price.value > 0){
+        if (model.value != '' && year.value != '' && description.value != '' && price.value != '') {
+            if (year.value > 0 && price.value > 0) {
                 let tr = create('tr');
                 tr.classList.add('info');
-                let tdProduct = create('td',model.value,tr);
-                let tdPrice = create('td',price.value,tr);
+                let tdProduct = create('td', model.value, tr);
+                let tdPrice = create('td', Number(price.value).toFixed(2), tr);
                 let tdBtns = create('td');
-                let btnInfo = create('button','More Info',tdBtns);
+                let btnInfo = create('button', 'More Info', tdBtns);
                 btnInfo.classList.add('moreBtn');
-                let btnBuy = create('button','Buy it',tdBtns);
+                let btnBuy = create('button', 'Buy it', tdBtns);
                 btnBuy.classList.add('buyBtn');
                 tr.appendChild(tdBtns);
                 tbody.appendChild(tr);
 
                 let trHide = create('tr');
                 trHide.classList.add('hide');
-                let tdYear = create('td',`Year: ${year.value}`,trHide);
-                let tdDescr = create('td',`Description: ${description.value}`,trHide);
-                tdDescr.colspan = 3;
+                let tdYear = create('td', `Year: ${year.value}`, trHide);
+                let tdDescr = create('td', `Description: ${description.value}`, trHide);
+
                 tbody.appendChild(trHide);
+
+                clear();
+
+                btnInfo.addEventListener('click', () => {
+                    if (btnInfo.textContent == 'More Info') {
+                        btnInfo.textContent = 'Less Info';
+                        trHide.style.display = 'contents';
+                        tdDescr.colSpan = 3;
+                    } else {
+                        btnInfo.textContent = 'More Info';
+                        trHide.style.display = 'none';
+                    }
+
+                });
+
+                btnBuy.addEventListener('click', (e) => {
+                    let parent = e.target.parentNode.parentNode;
+                    let price = parent.querySelector('tr.info td:nth-of-type(2)');
+                    totalSum += Number(price.textContent);
+                    totalPrice.textContent = totalSum.toFixed(2);
+                    parent.remove();
+
+                })
             }
         }
     })
