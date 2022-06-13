e10c0c0793d51f157b39645979339ac518349a0a
lakrizz1987 <ignatov.ivaylo@gmail.com>
2022-06-13 22:49:00 +0300

    project

--- Tic Tac Toe/app.js
+++ Tic Tac Toe/app.js
@@ -8,8 +8,9 @@ let c1Element = document.getElementById('c1');
 let c2Element = document.getElementById('c2');
 let c3Element = document.getElementById('c3');
 
-let winBoxElement = document.getElementById('xScore');
+let winBoxElement = document.getElementById('scoreBox');
 winBoxElement.style.display = 'none';
+
 let isOn = false;
 let startScreen = document.getElementById('chooseBox');
 let btnChoose = document.querySelectorAll('article .btnCh');
@@ -19,24 +20,101 @@ btnChoose.forEach(x => x.addEventListener('click', (e) => {
     startScreen.style.display = 'none';
 }))
 
-
-
+let count = 0;
+let isDraw = false;
 let mark = '';
 
 
+let audio = document.getElementById('audio');
+let btn = document.querySelector('button');
+
+btn.addEventListener('click', () => {
+    if (!isOn) {
+        audio.play()
+        btn.textContent = 'Reset Game';
+        startScreen.style.display = 'flex';
+        isOn = true;
+        a1Element.addEventListener('click', win);
+        a2Element.addEventListener('click', win);
+        a3Element.addEventListener('click', win);
+        b1Element.addEventListener('click', win);
+        b2Element.addEventListener('click', win);
+        b3Element.addEventListener('click', win);
+        c1Element.addEventListener('click', win);
+        c2Element.addEventListener('click', win);
+        c3Element.addEventListener('click', win);
+        isOn = true;
+
+    } else {
+        btn.textContent = 'Play Game';
+        document.querySelectorAll('.small-boxes').forEach(x => {
+            x.textContent = '';
+            winBoxElement.style.display = 'none';
+        })
+        isOn = false;
+    }
+})
+
+function checkWin(e) {
+    if (isDraw) {
+        winBoxElement.textContent = `DRAW !!!`;
+        winBoxElement.style.display = 'flex';
+        document.querySelectorAll('.small-boxes').forEach(x => {
+            x.removeEventListener('click', win);
+        })
+    } else {
+        mark == 'X' ? winBoxElement.textContent = `X Wins !!!` : winBoxElement.textContent = `O Wins !!!`;
+        winBoxElement.style.display = 'flex';
+        document.querySelectorAll('.small-boxes').forEach(x => {
+            x.removeEventListener('click', win);
+        })
+    }
+}
 
 function win(e) {
     if (e.target.textContent === '') {
         e.target.textContent = mark;
-        if (a1Element.textContent == a2Element.textContent && a1Element.textContent == a3Element.textContent) {
+        count++;
+        if ((a1Element.textContent == a2Element.textContent) && (a1Element.textContent == a3Element.textContent)) {
+            if (a1Element.textContent != '' && a2Element.textContent != '' && a3Element.textContent != '') {
+                checkWin()
+            }
+        } else if ((b1Element.textContent == b2Element.textContent) && (b1Element.textContent == b3Element.textContent)) {
+            if (b1Element.textContent != '' && b2Element.textContent != '' && b3Element.textContent != '') {
+                checkWin()
+            }
+        } else if ((c1Element.textContent == c2Element.textContent) && (c1Element.textContent == c3Element.textContent)) {
+            if (c1Element.textContent != '' && c2Element.textContent != '' && c3Element.textContent != '') {
+                checkWin()
+            }
+        } else if ((a1Element.textContent == b1Element.textContent) && (c1Element.textContent == a1Element.textContent)) {
+            if (a1Element.textContent != '' && b1Element.textContent != '' && c1Element.textContent != '') {
+                checkWin()
+            }
+        } else if ((a2Element.textContent == b2Element.textContent) && (c2Element.textContent == a2Element.textContent)) {
+            if (a2Element.textContent != '' && b2Element.textContent != '' && c2Element.textContent != '') {
+                checkWin()
+            }
+        } else if ((a3Element.textContent == b3Element.textContent) && (c3Element.textContent == a3Element.textContent)) {
+            if (a3Element.textContent != '' && b3Element.textContent != '' && c3Element.textContent != '') {
+                checkWin()
+            }
+        } else if ((a1Element.textContent == b2Element.textContent) && (a1Element.textContent == c3Element.textContent)) {
+            if (a1Element.textContent != '' && b2Element.textContent != '' && c3Element.textContent != '') {
+                checkWin()
+            }
+        } else if ((a3Element.textContent == b2Element.textContent) && (a3Element.textContent == c1Element.textContent)) {
+            if (a3Element.textContent != '' && b2Element.textContent != '' && c1Element.textContent != '') {
+                checkWin()
+            }
+        } else {
+            if (count == 9) {
+                isDraw = true;
+                checkWin()
+            }
 
-            mark == 'X' ? winBoxElement.textContent = `X Wins !!!` : winBoxElement.textContent = `O Wins !!!`;
-            winBoxElement.style.display = 'flex';
-            document.querySelectorAll('.small-boxes').forEach(x => {
-                x.removeEventListener('click', win)
-            })
         }
-    
+
         if (mark == 'X') {
             mark = 'O';
         } else {
@@ -45,35 +123,3 @@ function win(e) {
     }
 
 }
-
-
-let audio = document.getElementById('audio');
-let btn = document.querySelector('button');
-
-btn.addEventListener('click', () => {
-    
-    if (!isOn) {
-        audio.play()
-        btn.textContent = 'Reset Game';
-        startScreen.style.display = 'flex';
-        isOn = true;
-        a1Element.addEventListener('click', win)
-        a2Element.addEventListener('click', win)
-        a3Element.addEventListener('click', win)
-        b1Element.addEventListener('click', win)
-        b2Element.addEventListener('click', win)
-        b3Element.addEventListener('click', win)
-        c1Element.addEventListener('click', win)
-        c2Element.addEventListener('click', win)
-        c3Element.addEventListener('click', win)
-
-    } else {
-        btn.textContent = 'Play Game';
-        document.querySelectorAll('.small-boxes').forEach(x => {
-            x.textContent = '';
-            winBoxElement.style.display = 'none'
-        })
-        isOn = false;
-    }
-
-})
