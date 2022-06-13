a88e252015de9c447e9447162a4ad0893a1edbc2
Ivaylo <ignatov.ivaylo@gmail.com>
2022-06-13 16:33:13 +0300

    project

--- Tic Tac Toe/app.js
+++ Tic Tac Toe/app.js
@@ -8,60 +8,72 @@ let c1Element = document.getElementById('c1');
 let c2Element = document.getElementById('c2');
 let c3Element = document.getElementById('c3');
 
-let xScoreElement = document.getElementById('xScore');
-let yScoreElement = document.getElementById('yScore');
-let winBoxElement = document.getElementById('wins');
+let winBoxElement = document.getElementById('xScore');
+winBoxElement.style.display = 'none';
+let isOn = false;
+let startScreen = document.getElementById('chooseBox');
+let btnChoose = document.querySelectorAll('article .btnCh');
 
-let score = {
-    X: 0,
-    Y: 0
-}
+btnChoose.forEach(x => x.addEventListener('click', (e) => {
+    mark = e.target.textContent;
+    startScreen.style.display = 'none';
+}))
 
-let mark = 'X';
+
+
+let mark = '';
+
+
 
 function win(e) {
-    if (e.target.textContent == '') {
+    if (e.target.textContent === '') {
         e.target.textContent = mark;
+        if (a1Element.textContent == a2Element.textContent && a1Element.textContent == a3Element.textContent) {
+
+            mark == 'X' ? winBoxElement.textContent = `X Wins !!!` : winBoxElement.textContent = `O Wins !!!`;
+            winBoxElement.style.display = 'flex';
+            document.querySelectorAll('.small-boxes').forEach(x => {
+                x.removeEventListener('click', win)
+            })
+        }
+    
+        if (mark == 'X') {
+            mark = 'O';
+        } else {
+            mark = 'X';
+        }
     }
-
-    if (a1Element.textContent == a2Element.textContent && a1Element.textContent == a3Element.textContent) {
-        score[mark]++
-
-        winBoxElement.textContent = `${mark} WINS !!!`;
-        winBoxElement.hidden = false;
-
-    }
-
-    if (mark == 'X') {
-        mark = 'O';
-    } else {
-        mark = 'X';
-    }
-
 
 }
-a1Element.addEventListener('click', win)
-a2Element.addEventListener('click', win)
-a3Element.addEventListener('click', win)
-b1Element.addEventListener('click', win)
-b2Element.addEventListener('click', win)
-b3Element.addEventListener('click', win)
-
-
-
-
-
-
-
-
-
 
 
 let audio = document.getElementById('audio');
-let btn = document.querySelector('button')
-btn.addEventListener('click', () => {
-    audio.play()
-    btn.textContent = 'Reset Game';
+let btn = document.querySelector('button');
 
+btn.addEventListener('click', () => {
+    
+    if (!isOn) {
+        audio.play()
+        btn.textContent = 'Reset Game';
+        startScreen.style.display = 'flex';
+        isOn = true;
+        a1Element.addEventListener('click', win)
+        a2Element.addEventListener('click', win)
+        a3Element.addEventListener('click', win)
+        b1Element.addEventListener('click', win)
+        b2Element.addEventListener('click', win)
+        b3Element.addEventListener('click', win)
+        c1Element.addEventListener('click', win)
+        c2Element.addEventListener('click', win)
+        c3Element.addEventListener('click', win)
+
+    } else {
+        btn.textContent = 'Play Game';
+        document.querySelectorAll('.small-boxes').forEach(x => {
+            x.textContent = '';
+            winBoxElement.style.display = 'none'
+        })
+        isOn = false;
+    }
 
 })
